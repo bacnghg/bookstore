378e75c2fb9f904142a202416f70d73b63128acb
bacnghg <nhb.fit.hut@gmail.com>
2016-06-29 21:19:35 +0700

    add

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,7 +9,7 @@ class ReviewsController < ApplicationController
 	def create
 		@review = Review.new(review_params)
 		@review.read_id= @read.id 
-		@review.user_id = current_user&.id
+		@review.user_id = current_user.id
 
 		if @review.save
 			redirect_to read_path(@read)
